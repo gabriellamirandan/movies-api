cd9c956d060a5978eeae20f0a0097846cac8b948
gabriellamirandan <gabriella-miranda@hotmail.com>
2025-03-02 14:13:15 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/api/movies', (req, res) => {
+    const movies = [{
+        _id: '123456',
+        name: 'Ainda Estou Aqui',
+        year: 2024,
+        directors: ['Walter Salles'],
+        cast: ['Fernanda Torres', 'Fernanda Montenegro', 'Selton Mello'],
+        country: 'Brazil',
+        synopsis: 'lorem ipsum',
+        mpaa: 'PG-13'
+    },
+    {
+        _id: '123456',
+        name: 'Ainda Estou Aqui',
+        year: 2024,
+        directors: ['Walter Salles'],
+        cast: ['Fernanda Torres', 'Fernanda Montenegro', 'Selton Mello'],
+        country: 'Brazil',
+        synopsis: 'lorem ipsum',
+        mpaa: 'PG-13'
+    }];
+    res.json(movies)
+})
+
+app.get('/api/movies/:name', (req, res) => {
+    const movie = {
+        _id: '123456',
+        name: 'Ainda Estou Aqui',
+        year: 2024,
+        directors: ['Walter Salles'],
+        cast: ['Fernanda Torres', 'Fernanda Montenegro', 'Selton Mello'],
+        country: 'Brazil',
+        synopsis: 'lorem ipsum',
+        mpaa: 'PG-13'
+    };
+    res.json(movie)
+})
+
+app.post('/api/movies', (req, res) => {
+    res.sendStatus(201);
+})
+
+app.listen(port, () => {
+    console.log(`App de exemplo esta rodando na porta ${port}`)
+})
+
+// https://pt.wikipedia.org/wiki/REST
