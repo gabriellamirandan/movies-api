b16a8e56abda652c1c2f1003bb4771e4d9ec4e34
gabriellamirandan <gabriella-miranda@hotmail.com>
2025-04-02 23:20:43 +0100

    added post route to add a new movie to database

--- index.js
+++ index.js
@@ -3,25 +3,31 @@ const { MongoClient } = require('mongodb');
 
 const port = 3000;
 const mongoUrl = 'mongodb://localhost:27017';
-const dbName = 'movies';
+const dbName = 'projeto-movies';
+const collectionName = 'movies';
 
 const app = express();
 const client = new MongoClient(mongoUrl);
 
+//Function to connect with database
 const connectDb = async () => {
     await client.connect();
     console.log("Database connected")
 }
 
+//Function to disconnect with database
 const disconnectDb = async () => {
     await client.close();
     console.log("Database disconnected")
 }
 
+//Calling function to connect with database. Remember to start Docker.
 connectDb();
 
-//Define a pasta publica
+//Defines public folder
 app.use(express.static("public"));
+//Middleware que converte o body da requisicao em objeto js e para disponibiliza-lo na req.body
+app.use(express.json());
 
 app.get('/api/movies', (req, res) => {
     const movies = [{
@@ -61,8 +67,38 @@ app.get('/api/movies/:name', (req, res) => {
     res.json(movie);
 })
 
-app.post('/api/movies', (req, res) => {
-    res.sendStatus(201);
+app.post('/api/movies', async (req, res) => {
+    //dados no req.body
+    const name = req.body.name;
+    const year = req.body.year;
+    const mpaa = req.body.mpaa;
+    //validar nome, ano, mpaa
+    if (
+        name === "" ||
+        year < 1888 ||
+        mpaa === ""
+    ) {
+        // se nao tiver, retornar 400 (bad request)
+        res.sendStatus(400);
+    } else {
+        //pegar o banco de dados e a collection movies dentro do banco
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+
+        //inserir no banco usando insertOne
+        const insertedMovie = await collection.insertOne({
+            name,
+            year,
+            directors: req.body.directors,
+            cast: req.body.cast,
+            country: req.body.country,
+            synopsis: req.body.synopsis,
+            mpaa
+        });
+        //testar o resultado. O resultado retorna o insertedId que vamos usar para informar o filme adicionado.
+        console.log('Inserted documents =>', insertedMovie);
+        return res.sendStatus(201);
+    }
 })
 
 app.listen(port, () => {
